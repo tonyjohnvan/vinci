c0a3b9990abd87f99a41069ecb312ef181ed78a3
Fan Zhang <tonyjohnvan@gmail.com>
2015-05-25 19:24:37 -0400

    Finished the main Game Logic

--- ColorGame/assets/js/gameLogic.js
+++ ColorGame/assets/js/gameLogic.js
@@ -5,30 +5,126 @@
 // ranging from 0 to 62, active cells
 
 /* - game board layout -
- 0  1  2  3  4  5  6  7
- 8  9 10 11 12 13 14 15
- 16 17 18 19 20 21 22 23
- 24 25 26 27 28 29 30 31
- 32 33 34 35 36 37 38 39
- 40 41 42 43 44 45 46 47
- 48 49 50 51 52 53 54 55
- 56 57 58 59 60 61 62 63
+ 0  1  2  3  4  5  6
+ 7  8  9  10 11 12 13
+ 14 15 16 17 18 19 20
+ 21 22 23 24 25 26 27
+ 28 29 30 31 32 33 34
+ 35 36 37 38 39 40 41
+ 42 43 44 45 46 47 48
+ 49 50 51 52 53 54 55
+ 56 57 58 59 60 61 62
  */
 var levels = [
     {
-        'stage': 0,
-        'active': [27, 35, 34, 42, 43, 44, 36, 28, 29, 30]
+        'stage': 1,
+        'active': [24, 31, 30, 29, 36, 37, 38, 39, 40, 33],
+        'startPos': '24'
     },
+    {
+        'stage': 2,
+        'active': [9,10,11,12,13,20,18,17,16,23,24,25,26,32,33],
+        'startPos': '32'
+    },
+    {
+        'stage': 3,
+        'active': [50,43,36,37,44,51,52,45,38,39,40],
+        'startPos': '50'
+    },
+    {
+        'stage': 4,
+        'active': [31,32,33,30,29,24,25,26,23,37,38,22],
+        'startPos': '31'
+    },
+    {
+        'stage': 5,
+        'active': [15,16,17,25,24,23,30,29,36,37,38,31,32],
+        'startPos': '23'
+    },
+    {
+        'stage': 6,
+        'active': [51,44,37,30,23,24,31,38,45,52,53,54,47,40,33,39],
+        'startPos': '51'
+    }
 ];
 
 var currentLevel = 0;
 
+var timeUsed;
+
 function setGameBoard(level) {
+
+    $('.game-container').css({
+        'background': 'url(assets/img/color' + levels[currentLevel].stage + '.png) no-repeat center center',
+        'background-size': 'auto 100% '
+    });
     var cells = $('.gameboard').children();
 
     for (var actIn in level.active) {
-        $(cells[level.active[actIn-1]]).css({
-            'opacity':'1'
-        })
+        $(cells[level.active[actIn]]).attr('status', 'open').css({
+            'opacity': '1'
+        });
     }
+
+    $(cells[level.startPos]).attr('status', 'active').css({
+        'background': 'url(assets/img/activeTile' + levels[currentLevel].stage + '.png) no-repeat center center',
+        'background-size': '45px auto'
+    })
+
+}
+
+function updateBoard(cell) {
+    var cells = $('.gamespace[status]');
+    for (var ind = 0; ind < cells.length; ind++) {
+        if ($(cells[ind]).attr('status') == 'passed') {
+            $(cells[ind]).css({
+                'background': 'url(assets/img/Tile.png) no-repeat center center',
+                'background-size': '45px auto'
+            })
+        } else if ($(cells[ind]).attr('status') == 'active') {
+            $(cells[ind]).css({
+                'background': 'url(assets/img/activeTile' + levels[currentLevel].stage + '.png) no-repeat center center',
+                'background-size': '45px auto'
+            })
+        }
+    }
+}
+
+function resetGamePad() {
+    $('.gamespace').css({
+        'background': 'url(assets/img/cell.png) no-repeat center center',
+        'background-size': '50px 50px',
+        'opacity':0
+    }).removeAttr('status');
+    var cells = $('.gameboard').children();
+    $(cells[levels[currentLevel].startPos]).attr('status', 'active').css({
+        'background': 'url(assets/img/activeTile' + levels[currentLevel].stage + '.png) no-repeat center center',
+        'background-size': '45px auto'
+    });
+}
+
+function canMove(cell) {
+    return ( (cell.attr('status') == 'open'
+        || cell.attr('status') == 'active')
+        && checkAround(cell) );
+}
+
+function checkAround(cell) {
+    var cells = $('.gameboard').children();
+    return (
+        //check left
+        ($(cells[parseInt(cell.attr("id")) - 1]).attr('status') == 'active')
+        //check right
+        || ($(cells[parseInt(cell.attr("id")) + 1]).attr('status') == 'active')
+        //check up
+        || ($(cells[parseInt(cell.attr("id")) - 7]).attr('status') == 'active')
+        //check down
+        || ($(cells[parseInt(cell.attr("id")) + 7]).attr('status') == 'active')
+        //check self
+//        || ($(cells[parseInt(cell.attr("id"))]).attr('status') == 'active')
+        )
+}
+
+function checkIfWin(){
+    return ($('.gamespace[status="open"]').length==0)
 }
--- ColorGame/assets/js/index.js
+++ ColorGame/assets/js/index.js
@@ -2,25 +2,57 @@
  * Created by fanzhang on 5/25/15.
  */
 $(function(){
-    $('.gamespace').on('mouseover',function(e){
-//        console.log(e.target)
-        var cell = $(e.target);
-        cell.css({
-            'background': 'url(assets/img/Tile.png) no-repeat center center',
-            'background-size': '45px auto'
-        })
-    });
-    $('body').on('click',function(){
-        resetGamePad();
-    })
 
-    //setting up game
-    setGameBoard(levels[0]);
+    var startTime = new Date();
+
+    $('.gamespace').on('mouseover',function(e){
+//        console.log(e.target);
+        var cell = $(e.target);
+        if(canMove(cell)){
+            // make a move
+            $('.gamespace[status="active"]').attr('status','passed');
+            cell.attr('status','active');
+            updateBoard(cell);
+            if (checkIfWin()){
+                // Win Animation
+                var endTime = new Date();
+                timeUsed = (endTime - startTime)/1000;
+
+                //next level show
+                $('.gameboard').removeClass('animated zoomIn').addClass('animated zoomOut');
+                $('.timeUsed').html(timeUsed+'s');
+                setTimeout(function(){
+                    $('.gameboard').hide();
+                    $('.nextLevel').removeClass('animated zoomOut').show().addClass('animated zoomIn');
+                },500);
+            }
+        }
+    });
+
+    startLevel();
+
+    $('body').on('click',function(){
+        startLevel()
+    });
+
+
+    $('.continueGame').on('click',function(){
+
+        //next level show
+        $('.nextLevel').removeClass('animated zoomIn').addClass('animated zoomOut');
+        setTimeout(function(){
+            $('.nextLevel').hide();
+            $('.gameboard').removeClass('animated zoomOut').show().addClass('animated zoomIn');
+        },500);
+        setTimeout(function(){
+            startTime = new Date();
+        },500);
+        currentLevel++;
+        startLevel();
+    });
 });
 
-function resetGamePad(){
-    $('.gamespace').css({
-        'background': 'url(assets/img/cell.png) no-repeat center center',
-        'background-size': '50px 50px'
-    })
+function startLevel() {
+    resetGamePad();
+    setGameBoard(levels[currentLevel]);
 }
